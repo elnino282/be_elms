37767815ecddefa6985ce05074f6ef6aaf4c05fc
elnino282 <23521333@gm.uit.edu.vn>
2025-11-04 23:25:33 +0700

    initial commit

---
+++ src/main/java/org/example/be_elms/BeElmsApplication.java
@@ -0,0 +1,13 @@
+package org.example.be_elms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeElmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeElmsApplication.class, args);
+    }
+
+}
